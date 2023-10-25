ef8c85a080f9cff8f4405ba2776cf24aa2decf3c
MandyyChen <mandychen022@gmail.com>
2023-10-25 10:15:32 -0400

    10/25

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const DOMSelectors = {
     //select the textbox
     //select ALL the h2s in one property
 };
-DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    //const Album = makeAlbum()
-    //addCard(Album)
-    document
-    .insertAdjacentHTML(` <input type="text" name="firstName" class="first-name"/>`
+    
 
-    );
+DOMSelectors.form.addEventListener("submit", function () {
+    let firstName = DOMSelectors.firstName.value;
+    DOMSelectors.document.insertAdjacentHTML(<input type="text" name="firstName" class="first-name"> ${DOMSelectors.firstName.value}` </h2>),
 });
+    console.log(DOMSelectors.firstName.value);
+
 /*
     DOMSelectors.h2s.forEach(
-        (el) => (el.textContext = DOMSelectors.firstName.value)
+        (el) => (el.textContext = DOMSelectors.firstName.value));
+//const Album = makeAlbum()
+    //addCard(Album)
+});
+
+     document
+    .insertAdjacentHTML(` <input type="text" name="firstName" class="first-name"/>`
+   
     ); */
